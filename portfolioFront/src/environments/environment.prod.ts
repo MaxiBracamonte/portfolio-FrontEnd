3f69953b814d57f850e2470a09b1d5ff5ed06c98
MaxiBracamonte <104867774+MaxiBracamonte@users.noreply.github.com>
2022-07-29 01:55:13 -0300

    firebase

--- portfolioFront/src/environments/environment.prod.ts
+++ portfolioFront/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiSobreMi: 'https://primerporfolioweb.herokuapp.com/auth/contenedores/',
-  apiExp: 'https://primerporfolioweb.herokuapp.com/auth/experiencias/',
-  apiEdu: 'https://primerporfolioweb.herokuapp.com/auth/educaciones/',
-  apiPro: 'https://primerporfolioweb.herokuapp.com/auth/proyectos/',
-  apiHabi: 'https://primerporfolioweb.herokuapp.com/auth/habilidades/',
-  apiPer: 'https://primerporfolioweb.herokuapp.com/auth/perfiles/',
-  authUrl: 'https://primerporfolioweb.herokuapp.com/auth/'
+  apiSobreMi: 'https://portfolioapback.herokuapp.com/auth/contenedores/',
+  apiExp: 'https://portfolioapback.herokuapp.com/auth/experiencias/',
+  apiEdu: 'https://portfolioapback.herokuapp.com/auth/educaciones/',
+  apiPro: 'https://portfolioapback.herokuapp.com/auth/proyectos/',
+  apiHabi: 'https://portfolioapback.herokuapp.com/auth/habilidades/',
+  apiPer: 'https://portfolioapback.herokuapp.com/auth/perfiles/',
+  authUrl: 'https://portfolioapback.herokuapp.com/auth/'
 };
